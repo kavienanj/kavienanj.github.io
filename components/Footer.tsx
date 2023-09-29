e6d75bc5a8412d6cfe6c5cc587f644abe98f4b40
Jegatheesan Kavienan <kavienanj@gmail.com>
2023-09-29 16:41:26 +0530

    init version nextjs 13 project

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-auto w-full h-24 border-solid border-t border-zinc-200 dark:border-zinc-800 flex justify-center items-center">
-      Created with&nbsp;<b><a href="https://typescriptlang.org">Typescript</a>, <a href="https://nextjs.org">Next.js</a> & <a href="https://tailwindcss.com">Tailwind</a></b>&nbsp;⚡️
-    </footer>
-  );
-}
