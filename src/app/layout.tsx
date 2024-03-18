363f280d65b569849d5788c37f0a003b106a8fd9
Jegatheesan Kavienan <kavienanj@gmail.com>
2024-03-18 18:02:33 +0530

    meta data and favicon change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const meslo = Inconsolata({
 
 export const metadata: Metadata = {
   title: 'Kavienan J | Full-Stack Developer',
-  description: 'Explore my work on Flutter, Next.js, Firebase, Figma, LLMs and more.',
+  description: 'Build bleeding edge applications with me! I help businesses build and deploy scalable applications to grow their business.',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
